4d0c9acbcee48caa58e2eb1fac4f7b76e861c7ed
Peter Donald <peter@realityforge.org>
2016-12-06 10:40:18 +1100

    Add in basic facet definition

---
+++ lib/reality/facets/facet.rb
@@ -0,0 +1,77 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Reality #nodoc
+  module Facets #nodoc
+
+    class Facet < Reality::BaseElement
+      attr_reader :facet_container
+      attr_reader :key
+      attr_accessor :required_facets
+      attr_accessor :suggested_facets
+
+      def initialize(facet_container, key, options = {}, &block)
+        options = options.dup
+        @key = key
+        @facet_container = facet_container
+        @required_facets = []
+        @suggested_facets = []
+        facet_container.send :register_facet, self
+        super(options, &block)
+        facet_container.target_manager.targets.each do |target|
+          target.extension_module.class_eval <<-RUBY
+            def #{self.key}?
+              !!@#{self.key}_facet_enabled
+            end
+
+            def _enable_facet_#{self.key}!
+              @#{self.key}_facet_enabled = true
+            end
+
+            def _disable_facet_#{self.key}!
+              @#{self.key}_facet_enabled = false
+              @facet_#{self.key} = nil
+            end
+          RUBY
+        end
+      end
+
+      def enhance(model_class, &block)
+        target_manager = facet_container.target_manager
+        target = target_manager.target_by_model_class(model_class)
+
+        extension_name = "#{::Reality::Naming.pascal_case(self.key)}#{model_class.name.gsub(/^.*\:\:([^\:]+)/, '\1')}Facet"
+        definitions = target_manager.container.facet_definitions
+        if target.key
+          definitions.class_eval "class #{extension_name} < Reality.base_element(:container_key => :#{target.key}); end"
+          extension_instance = definitions.const_get(extension_name)
+          extension_instance.class_eval(&block)
+
+          model_extension = target.extension_module
+          model_extension.class_eval <<-RUBY
+            def #{self.key}
+              self.facet_#{self.key}
+            end
+
+            def facet_#{self.key}
+              raise "Attempted to access '#{self.key}' facet for model '#{model_class.name}' when facet disabled." unless #{self.key}? 
+              @facet_#{self.key} ||= #{extension_instance.name}.new(self)
+            end
+          RUBY
+        end
+      end
+    end
+  end
+end
+
---
+++ test/facets/test_facet.rb
@@ -0,0 +1,67 @@
+require File.expand_path('../../helper', __FILE__)
+
+class Reality::Facets::TestFacet < Reality::TestCase
+  class Component < Reality.base_element(:name => true, :container_key => :project)
+  end
+
+  class Project < Reality.base_element(:name => true)
+    def component(name, options = {}, &block)
+      component_map[name.to_s] = Component.new(self, name, options, &block)
+    end
+
+    def components
+      component_map.values
+    end
+
+    def component_map
+      @component_map ||= {}
+    end
+  end
+
+  def test_basic_operation
+
+    assert_equal false, TestFacetContainer.facet_by_name?(:gwt)
+    assert_equal false, TestFacetContainer.facet_by_name?(:gwt_rpc)
+    assert_equal false, TestFacetContainer.facet_by_name?(:imit)
+
+    TestFacetContainer.target_manager.target(Project, :project)
+    TestFacetContainer.target_manager.target(Component, :component, :project, :access_method => 'comps')
+
+    Reality::Facets::Facet.new(TestFacetContainer, :gwt)
+    Reality::Facets::Facet.new(TestFacetContainer, :gwt_rpc, :required_facets => [:gwt])
+
+    Reality::Facets::Facet.new(TestFacetContainer, :imit, :suggested_facets => [:gwt_rpc]) do |f|
+      f.enhance(Project) do
+        def name
+          "Gwt#{project.name}"
+        end
+      end
+    end
+
+    assert_equal true, TestFacetContainer.facet_by_name?(:gwt)
+    assert_equal true, TestFacetContainer.facet_by_name?(:gwt_rpc)
+    assert_equal true, TestFacetContainer.facet_by_name?(:imit)
+
+    project = Project.new(:MyProject) do |project|
+      target = TestFacetContainer.target_manager.target_by_model_class(project.class)
+      project.class.include target.extension_module if target.extension_module?
+      project._enable_facet_gwt!
+      project._enable_facet_gwt_rpc!
+      project._enable_facet_imit!
+      project.component(:MyComponent) do |component|
+        target = TestFacetContainer.target_manager.target_by_model_class(component.class)
+        component.class.include target.extension_module if target.extension_module?
+      end
+    end
+    assert_equal true, project.gwt?
+    assert_equal false, project.respond_to?(:gwt)
+    assert_equal false, project.respond_to?(:facet_gwt)
+    assert_equal true, project.gwt_rpc?
+    assert_equal false, project.respond_to?(:gwt_rpc)
+    assert_equal false, project.respond_to?(:facet_gwt_rpc)
+    assert_equal true, project.imit?
+    assert_equal true, project.respond_to?(:imit)
+    assert_equal true, project.respond_to?(:facet_imit)
+    assert_equal 'GwtMyProject', project.imit.name
+  end
+end
